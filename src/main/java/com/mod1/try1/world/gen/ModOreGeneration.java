8e20f8558cc7af01770f3ca9a4c56f78260e53ca
kelvin104 <choimanhin9526@gmail.com>
2021-10-07 12:46:39 +0800

    -Doing; ore gen -next: weather/ biomes, mask, infection system

--- src/main/java/com/mod1/try1/world/gen/ModOreGeneration.java
+++ src/main/java/com/mod1/try1/world/gen/ModOreGeneration.java
@@ -1,6 +1,7 @@
 package com.mod1.try1.world.gen;
 
 import com.google.common.collect.ImmutableList;
+import com.mod1.try1.Main;
 import com.mod1.try1.block.ModBlocks;
 import net.minecraft.core.Registry;
 import net.minecraft.data.BuiltinRegistries;
@@ -51,13 +52,14 @@ public class ModOreGeneration {
 
     public static void registerConfiguredFeatures() {
         MOD_ORE_TARGET_LIST = ImmutableList.of(OreConfiguration.target(OreConfiguration.Predicates.STONE_ORE_REPLACEABLES, ((Block) ModBlocks.ITEM_ONE_ORE.get()).defaultBlockState()), OreConfiguration.target(OreConfiguration.Predicates.STONE_ORE_REPLACEABLES, ((Block) ModBlocks.ITEM_ONE_ORE.get()).defaultBlockState()));
-        MOD_ORE = (ConfiguredFeature)((ConfiguredFeature)((ConfiguredFeature) Feature.ORE.configured(new OreConfiguration(MOD_ORE_TARGET_LIST, (Integer) ModOreConfig.MOD_ORE_SIZE.get())).rangeUniform(VerticalAnchor.absolute((Integer) ModOreConfig.MOD_ORE_MIN_HEIGHT.get()), VerticalAnchor.absolute((Integer) ModOreConfig.MOD_ORE_MAX_HEIGHT.get()))).squared()).count((Integer) ModOreConfig.MOD_ORE_CHANCE.get());
-        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new ResourceLocation("examplemod", "example_ore"), MOD_ORE);
+        MOD_ORE = (ConfiguredFeature)( (ConfiguredFeature)((ConfiguredFeature) Feature.ORE.configured(new OreConfiguration(MOD_ORE_TARGET_LIST, (Integer) ModOreConfig.MOD_ORE_SIZE.get())).rangeUniform(VerticalAnchor.absolute((Integer) ModOreConfig.MOD_ORE_MIN_HEIGHT.get()), VerticalAnchor.absolute((Integer) ModOreConfig.MOD_ORE_MAX_HEIGHT.get()))).squared()).count((Integer) ModOreConfig.MOD_ORE_CHANCE.get());
+        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new ResourceLocation(Main.MOD_ID, "mod_ore"), MOD_ORE);
     }
 
     @SubscribeEvent(priority = EventPriority.HIGH)
     public static void registerBiomeModification(BiomeLoadingEvent event) {
         //event.getGeneration().getFeatures(GenerationStep.Decoration.UNDERGROUND_ORES).add(() -> {
+        registerConfiguredFeatures();
         event.getGeneration().getFeatures(GenerationStep.Decoration.UNDERGROUND_ORES).add(() -> {
             return MOD_ORE;
         });
